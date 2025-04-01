2c72065053310d883e964379102a8f5f5161fc21
Jose Andrés Guzmán <103334704+JoseanG24@users.noreply.github.com>
2025-04-01 13:52:58 -0600

    removed image from register

--- app/registro/page.tsx
+++ app/registro/page.tsx
@@ -5,7 +5,6 @@ import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
-import Image from "next/image";
 import { useRouter } from "next/navigation";
 import gsap from "gsap";
 
